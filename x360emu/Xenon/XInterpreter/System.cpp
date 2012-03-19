79519e18ce0acb9dbca4642140669aaa08d7afd2
Francesco Boffa <alfaomega08@gmail.com>
2012-03-19 23:16:53 +0100

    First test executable runt successfully from start to end!

--- x360emu/Xenon/XInterpreter/System.cpp
+++ x360emu/Xenon/XInterpreter/System.cpp
@@ -14,96 +14,96 @@ using namespace std;
 
 void XInterpreter::OpCrand(Xenon::CpuState *xState)
 {
-    xState->SetCrBit(INSTR.CRBD, xState->GetCrBit(INSTR.CRBA) & xState->GetCrBit(INSTR.CRBB));
+	xState->SetCrBit(INSTR.CRBD, xState->GetCrBit(INSTR.CRBA) & xState->GetCrBit(INSTR.CRBB));
 }
 
 void XInterpreter::OpCrandc(Xenon::CpuState *xState)
 {
-    xState->SetCrBit(INSTR.CRBD, xState->GetCrBit(INSTR.CRBA) & (1 ^ xState->GetCrBit(INSTR.CRBB)));
+	xState->SetCrBit(INSTR.CRBD, xState->GetCrBit(INSTR.CRBA) & (1 ^ xState->GetCrBit(INSTR.CRBB)));
 }
 
 void XInterpreter::OpCreqv(Xenon::CpuState *xState)
 {
-    xState->SetCrBit(INSTR.CRBD, 1 ^ (xState->GetCrBit(INSTR.CRBA) ^ xState->GetCrBit(INSTR.CRBB)));
+	xState->SetCrBit(INSTR.CRBD, 1 ^ (xState->GetCrBit(INSTR.CRBA) ^ xState->GetCrBit(INSTR.CRBB)));
 }
 
 void XInterpreter::OpCrnand(Xenon::CpuState *xState)
 {
-    xState->SetCrBit(INSTR.CRBD, 1 ^ (xState->GetCrBit(INSTR.CRBA) & xState->GetCrBit(INSTR.CRBB)));
+	xState->SetCrBit(INSTR.CRBD, 1 ^ (xState->GetCrBit(INSTR.CRBA) & xState->GetCrBit(INSTR.CRBB)));
 }
 
 void XInterpreter::OpCrnor(Xenon::CpuState *xState)
 {
-    xState->SetCrBit(INSTR.CRBD, 1 ^ (xState->GetCrBit(INSTR.CRBA) | xState->GetCrBit(INSTR.CRBB)));
+	xState->SetCrBit(INSTR.CRBD, 1 ^ (xState->GetCrBit(INSTR.CRBA) | xState->GetCrBit(INSTR.CRBB)));
 }
 
 void XInterpreter::OpCror(Xenon::CpuState *xState)
 {
-    xState->SetCrBit(INSTR.CRBD, (xState->GetCrBit(INSTR.CRBA) | xState->GetCrBit(INSTR.CRBB)));
+	xState->SetCrBit(INSTR.CRBD, (xState->GetCrBit(INSTR.CRBA) | xState->GetCrBit(INSTR.CRBB)));
 }
 
 void XInterpreter::OpCrorc(Xenon::CpuState *xState)
 {
-    xState->SetCrBit(INSTR.CRBD, (xState->GetCrBit(INSTR.CRBA) | (1 ^ xState->GetCrBit(INSTR.CRBB))));
+	xState->SetCrBit(INSTR.CRBD, (xState->GetCrBit(INSTR.CRBA) | (1 ^ xState->GetCrBit(INSTR.CRBB))));
 }
 
 void XInterpreter::OpCrxor(Xenon::CpuState *xState)
 {
-    xState->SetCrBit(INSTR.CRBD, (xState->GetCrBit(INSTR.CRBA) ^ xState->GetCrBit(INSTR.CRBB)));
+	xState->SetCrBit(INSTR.CRBD, (xState->GetCrBit(INSTR.CRBA) ^ xState->GetCrBit(INSTR.CRBB)));
 }
 
 void XInterpreter::OpMfmsr(Xenon::CpuState *xState)
 {
-    rGPR[INSTR.RD] = xState->msr;
+	rGPR[INSTR.RD] = xState->msr;
 }
 
 void XInterpreter::OpMfspr(Xenon::CpuState *xState)
 {
-    u32 iIndex = ((INSTR.SPR & 0x1F) << 5) + ((INSTR.SPR >> 5) & 0x1F);
-    rGPR[INSTR.RD] = rSPR[iIndex];
+	u32 iIndex = ((INSTR.SPR & 0x1F) << 5) + ((INSTR.SPR >> 5) & 0x1F);
+	rGPR[INSTR.RD] = rSPR[iIndex];
 }
 
 void XInterpreter::OpMftb(Xenon::CpuState *xState)
 {
-    int iIndex = (INSTR.TBR >> 5) | ((INSTR.TBR & 0x1F) << 5);
-    rGPR[INSTR.RD] = xState->spr[iIndex];
+	int iIndex = (INSTR.TBR >> 5) | ((INSTR.TBR & 0x1F) << 5);
+	rGPR[INSTR.RD] = xState->spr[iIndex];
 }
 
 void XInterpreter::OpMtmsrd(Xenon::CpuState *xState)
 {
-    if (INSTR.L_MSR)
-    {
-        xState->msr &= ~0xFFFE;
-        xState->msr |= rGPR[INSTR.RS] & 0xFFFE;
-    }
-    else
-    {
-        xState->msr &= ~0x6FFFFFFFFFFF6FCF;
-        xState->msr |= rGPR[INSTR.RS] & 0x6FFFFFFFFFFF6FCF;
+	if (INSTR.L_MSR)
+	{
+		xState->msr &= ~0xFFFE;
+		xState->msr |= rGPR[INSTR.RS] & 0xFFFE;
+	}
+	else
+	{
+		xState->msr &= ~0x6FFFFFFFFFFF6FCF;
+		xState->msr |= rGPR[INSTR.RS] & 0x6FFFFFFFFFFF6FCF;
 
-        xState->msr |= (rGPR[INSTR.RS] & 0xC000000000000000) ? 0x8000000000000000 : 0;
-        xState->msr |= (rGPR[INSTR.RS] & 0x4010) ? 0x10 : 0;
-    }
+		xState->msr |= (rGPR[INSTR.RS] & 0xC000000000000000) ? 0x8000000000000000 : 0;
+		xState->msr |= (rGPR[INSTR.RS] & 0x4010) ? 0x10 : 0;
+	}
 }
 
 void XInterpreter::OpMtspr(Xenon::CpuState *xState)
 {
-    u32 iIndex = (INSTR.SPRU << 5) | (INSTR.SPRL & 0x1F);
-    rSPR[iIndex] = rGPR[INSTR.RD];
+	u32 iIndex = (INSTR.SPRU << 5) | (INSTR.SPRL & 0x1F);
+	rSPR[iIndex] = rGPR[INSTR.RD];
 }
 
 void XInterpreter::OpTwi(Xenon::CpuState *xState)
 {
-    s64 a = rGPR[INSTR.RA];
-    s64 b = INSTR.SIMM_16;
-    s64 TO = INSTR.TO;
+	s64 a = rGPR[INSTR.RA];
+	s64 b = INSTR.SIMM_16;
+	s64 TO = INSTR.TO;
 
-    if ( ((a < b) && (TO & 0x10))
-        || ((a > b) && (TO & 0x08))
-        || ((a == b) && (TO & 0x04))
-        || (((u64) a < (u64) b) && (TO & 0x02))
-        || (((u64) a > (u64) b) && (TO & 0x01)))
-    {
-        throw Exception("Oh shit! It's a TRAP!");
-    }
+	if ( ((a < b) && (TO & 0x10))
+		|| ((a > b) && (TO & 0x08))
+		|| ((a == b) && (TO & 0x04))
+		|| (((u64) a < (u64) b) && (TO & 0x02))
+		|| (((u64) a > (u64) b) && (TO & 0x01)))
+	{
+		throw Exception("Oh shit! It's a TRAP!");
+	}
 }
